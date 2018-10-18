1dfd78a25ee9e2c7b6391d7cb2e1b92411f5ff48
olga <olga@compie.co.il>
2018-10-18 18:04:03 +0300

    first commit

---
+++ src/translations/en/app/browser.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin File Manager browser page lines
+    |--------------------------------------------------------------------------
+    */
+
+    'select_folder' => 'Select folder',
+
+    'buttons' => [
+        'upload_files' => 'Upload files',
+        'add_folder' => 'Add folder',
+    ],
+
+    'items' => 'Items',
+
+    'search_current_folder' => 'Search in current folder',
+
+    'no_files_to_display' => 'No files to display',
+
+    'files_were_selected' => 'Files were selected',
+    'images_were_selected' => 'Images were selected',
+
+    'choose_image' => 'Choose image',
+    'choose_file' => 'Choose file',
+
+    'actions' => [
+        'display' => 'פעולות',
+        'check_all' => 'בחר הכל',
+        'uncheck_all' => 'בטל הכל',
+        'selected' => 'פריטים נבחרו',
+        'remove' => [
+            'display' => 'מחק',
+            'are_you_sure' => 'האם אתה בטוח?'
+        ],
+        'or' => 'או',
+        'move' => 'העבר',
+    ],
+
+    'images' => 'תמונות',
+    'files'  => 'קבצים',
+    'image' => 'תמונה',
+    'file'  => 'קובץ',
+
+    'select_options_from' => 'הנך יכול לבחור בין: ',
+    'select_options_to' => ' ו- ',
+
+    'details' => [
+        'date' => 'תאריך: ',
+        'hour' => 'שעה: ',
+        'size' => 'גודל: ',
+    ],
+
+    'add_to_page' => 'הוסף לעמוד',
+
+    'new_folder' => 'תיקייה חדשה'
+
+];
---
+++ src/translations/en/app/main.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin File Manager Lines
+    |--------------------------------------------------------------------------
+    */
+
+
+    'title' => 'קבצים',
+
+    'main_folder' => 'תיקייה ראשית',
+
+    'main_title' => 'ענן קבצים',
+
+    'browser' => include('browser.php'),
+
+    'upload' => include('upload.php'),
+
+    'crop' => include('crop.php'),
+
+    'js_lines' => include('js-lines.php'),
+
+    'components' => include('components.php'),
+];
